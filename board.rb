0ac60ca79e9efe075d4e30e35a88540e37677a98
christinedo <christinevan.do@gmail.com>
2019-05-01 22:01:39 -0700

    add method to move pieces around the board

--- board.rb
+++ board.rb
@@ -17,6 +17,33 @@ class Board
     end
   end
 
+  def [](pos)
+    row, col = pos
+    rows[row][col]
+  end
+
+  def []=(pos, val)
+    row, col = pos
+    rows[row][col] = val
+  end
+
   def move_piece(start_pos, end_pos)
+    raise NoPieceAtPosition if self[start_pos].nil?
+    raise InvalidEndPosition if end_pos.any? { |num| num < 0 }
+
+    self[end_pos] = self[start_pos]
+    self[start_pos] = nil
+  end
+end
+
+class NoPieceAtPosition < StandardError
+  def message
+    "There's no piece at the given start position."
+  end
+end
+
+class InvalidEndPosition < ArgumentError
+  def message
+    "Given end position is out of range or otherwise not playable."
   end
 end
